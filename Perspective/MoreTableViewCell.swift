25d3e54bdba8024cafbf3a1e2c8db077a2c57258
Daniel Dunbar <Guy@Laras-MacBook-Pro.local>
2016-07-10 11:54:14 -0400

    First commit. Open source not yet added.

---
+++ Perspective/MoreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoreTableViewCell.swift
+//  Perspective
+//
+//  Created by Daniel Dunbar on 6/26/16.
+//  Copyright © 2016 Perspective Fitness. All rights reserved.
+//
+
+import UIKit
+
+class MoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
